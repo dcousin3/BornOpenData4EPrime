53b52b33efd50c5e18474879d61d23aa5486af1d
dcousin3 <denis.cousineau@uottawa.ca>
2019-09-10 08:20:33 -0400

    R library to import data stored by E-Prime

---
+++ BornOpenData.eprime2/R/BornOpenData.eprime2.R
@@ -0,0 +1,23 @@
+#' @title BornOpenData.eprime2
+#'
+#' @description
+#' This package has a function to read data stored as BornOpenData by E-Prime
+#' when complemented with the package BornOpenData4EPrime.
+#'
+#' @details
+#' BornOpenData.eprime2 is a package with a single function, 
+#' readBornOpenData.eprime2, meant to get data from an E-Prime 
+#' experiment made BornOpenData with the package BornOpenData4EPrime. 
+#' 
+#' readBornOpenData.eprime2(owner, repository, verbose = FALSE )  to read all the data on the repository.
+#' 
+#' where owner, repository points to a git hub repository that can be consulted at
+#'     https://github.com/owner/repository
+#' and the data files themselves are located in
+#'     https://github.com/owner/repository/rawdata
+#'
+#' See help(readBornOpenData.eprime2) for the function.
+#' 
+#' @keywords internal
+"_PACKAGE"
+#> [1] "_PACKAGE"
